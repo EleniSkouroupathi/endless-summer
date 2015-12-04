2192e0bf8ffb60cfcdd280bee0a346b3781b0966
Eleni Skouroupathi <eleni59@hotmail.com>
2015-12-04 11:16:22 +0000

    1efg

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -1,4 +1,16 @@
-class String
-
+def wavecase
+	word=gets.chomp
+word_length=word.length
+for i in (0 ... word_length) do
+   if i%2==0
+       puts word[i].upcase
+   else
+       puts word[i].downcase
+   end
+   
+   
+   
+end
 
 end
+wavecase
